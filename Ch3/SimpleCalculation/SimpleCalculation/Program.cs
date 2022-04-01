4e6e585e5ceeafaef4309ec9cceba0a4ed038547
pea-sys <pea98258@gmail.com>
2022-04-01 20:48:17 +0900

    simple_asm

---
+++ Ch3/SimpleCalculation/SimpleCalculation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation(4, 83);
+        }
+        static int Calculation(int a ,int b)
+        {
+            int x = a + b;
+            int y = a - b;
+            int z = b - a;
+            int w = 2 * b + 2 * a;
+            return x + y + z + w;
+        }
+    }
+}
