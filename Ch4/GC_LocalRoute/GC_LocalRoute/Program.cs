3fe97995c9598760e8436710333857f4a850370b
pea-sys <pea98258@gmail.com>
2022-04-06 21:36:56 +0900

    GC_LocalRoute

---
+++ Ch4/GC_LocalRoute/GC_LocalRoute/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace GC_LocalRoute
+{
+    internal class Program
+    {
+        /// <summary>
+        /// DEBUGモードだと最適化されないのでtimerはGCの対象にならない。動作し続ける
+        /// RELEASEモードだと最適化されるのでtimerはGCの対象になる。1回動作して解放される
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Timer timer = new Timer(OnTimer, null, 0, 1000);
+            Console.ReadLine();
+        }
+        static void OnTimer(object state)
+        {
+            Console.WriteLine(DateTime.Now.TimeOfDay);
+            GC.Collect();
+        }
+    }
+}
