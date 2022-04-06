a4b0e5c0fbd9c4337a0bbd7d779cb56fbc6b383d
pea-sys <pea98258@gmail.com>
2022-04-07 07:03:12 +0900

    Dump解析しやすいようにSleepを入れる

---
+++ Ch4/GC_StaticRoute/GC_StaticRoute/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GC_StaticRoute
+{
+    class Button
+    {
+        public void OnClick(object sender, EventArgs e)
+        {
+            
+        }
+    }
+    internal class Program
+    {
+        static event EventHandler ButtonClick;
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Button button = new Button();
+                ButtonClick += button.OnClick; //ハンドルメモリリーク
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
