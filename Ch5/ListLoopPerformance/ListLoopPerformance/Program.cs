7db6488cad38044bf78cb0a2c8247146ce89a4b7
pea-sys <pea98258@gmail.com>
2022-04-11 23:06:14 +0900

    Loop測定インライン展開防止

--- Ch5/ListLoopPerformance/ListLoopPerformance/Program.cs
+++ Ch5/ListLoopPerformance/ListLoopPerformance/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,11 +13,19 @@ namespace ListLoopPerformance
         /// <summary>
         /// WhileとGotoが早すぎるように見える。
         /// ベンチマークの取り方がおかしいかも。
-        /// For : 170msec
+        /// [32bit Process]
+        /// For : 42msec
         /// Foreach : 278msec
         /// List<T>.ForEach : 152msec
-        /// While : 1msec
-        /// GoTo : 1msec
+        /// While : 67msec
+        /// GoTo : 50msec
+        /// 
+        /// [64bit Process]
+        /// For : 43msec
+        /// Foreach : 405msec
+        /// List<T>.ForEach :246msec
+        /// While : 48msec
+        /// GoTo : 52msec
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
@@ -32,7 +41,7 @@ namespace ListLoopPerformance
             int OUTER_LOOP = 100;
             int INNER_LOOP = 50000000;
             long spent_time = 0;
-            int index = 0;
+
             Stopwatch sw = new Stopwatch();
 
             var list = Enumerable.Repeat("a", INNER_LOOP).ToList();
@@ -41,44 +50,23 @@ namespace ListLoopPerformance
                 sw.Restart();
                 if (mode == 0)
                 {
-                    for (index = 0; index < list.Count - 1; index++)
-                    {
-                        _ = list[index];
-                    }
+                    ForTest(list);
                 }
                 else if (mode == 1)
                 {
-                    foreach (string l in list)
-                    {
-                        _ = l;
-                    }
+                    ForeachTest(list);
                 }
                 else if (mode == 2)
                 {
-                    list.ForEach(x => { _ = x; });
+                    GenericForeachTest(list);
                 }
                 else if (mode == 3)
                 {
-                    while (list.Count > index)
-                    {
-                        _ = list[index++];
-                    }
+                    WhileTest(list);
                 }
                 else if (mode == 4)
                 {
-                    goto LOOPSTART;
-                LOOPSTART:
-                    if (index < list.Count)
-                    {
-                        _ = list[index++];
-                        goto LOOPSTART;
-                    }
-                    else
-                    {
-                        goto LOOPEND;
-                    }
-                LOOPEND:
-                    ;
+                    GoToTest(list);
                 }
                 spent_time += sw.ElapsedMilliseconds;
             }
@@ -86,6 +74,55 @@ namespace ListLoopPerformance
             spent_time /= OUTER_LOOP;
             Console.WriteLine($"{spent_time}msec");
             Console.ReadLine();
+        }
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void ForTest(List<string> list)
+        {
+            for (int index = 0; index < list.Count - 1; index++)
+            {
+                _ = list[index];
+            }
+        }
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void ForeachTest(List<string> list)
+        {
+            foreach (string l in list)
+            {
+                _ = l;
+            }
+        }
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void GenericForeachTest(List<string> list)
+        {
+            list.ForEach(x => { _ = x; });
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void WhileTest(List<string> list)
+        {
+            int index = 0;
+            while (list.Count > index)
+            {
+                _ = list[index++];
+            }
+        }
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void GoToTest(List<string> list)
+        {
+            int index = 0;
+            goto LOOPSTART;
+        LOOPSTART:
+            if (index < list.Count)
+            {
+                _ = list[index++];
+                goto LOOPSTART;
+            }
+            else
+            {
+                goto LOOPEND;
+            }
+        LOOPEND:
+            ;
         }
     }
 }
