fd4ee6e90976895016854c515765b4bf6943afd2
pea-sys <pea98258@gmail.com>
2023-03-17 07:18:40 +0900

    ListCapacity

---
+++ Original/ListCapacity/ListCapacity/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace ListCapacity
+{
+    [ShortRunJob]
+    [MinColumn, MaxColumn]
+    public class Measure
+    {
+        [Benchmark]
+        public void UseCapacityInt()
+        {
+            UseCapacity(10000000);
+        }
+        [Benchmark]
+        public void NoUseCapacityInt()
+        {
+            NoUseCapacity(1000000);
+        }
+        [Benchmark]
+        public void UseCapacityShort()
+        {
+            UseCapacity(short.MaxValue);
+        }
+        [Benchmark]
+        public void NoUseCapacityShort()
+        {
+            NoUseCapacity(short.MaxValue);
+        }
+        public void UseCapacity(int capacity)
+        {
+            List<int> list = new List<int>(capacity);
+            for (var i = 0; i < capacity; i++)
+            {
+                list.Add(i);
+            }
+            var s = 0;
+            foreach (var l in list)
+            {
+                s = l * 2;
+            }
+        }
+       
+        public void NoUseCapacity(int capacity)
+        {
+            List<int> list = new List<int>();
+            for (var i = 0; i < capacity; i++)
+            {
+                list.Add(i);
+            }
+            var s = 0;
+            foreach (var l in list)
+            {
+                s = l * 2;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Measure>();
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-# .net_performance_experiments 
-アセンブラレベルの知識も求められる
-「Pro .NET Performance」の学習用リポジトリ  
-
-本家GitHub : (https://github.com/Apress/pro-.net-perf)  
-書籍       : (https://link.springer.com/book/10.1007/978-1-4302-4459-2)  
-和訳版     : (https://www.shoeisha.co.jp/book/detail/9784798131795)
+# .net-performance-experiments
