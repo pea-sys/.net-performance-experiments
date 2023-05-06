3d0982066a77b303ebf0eb7d1dd17dc7ea4061c7
pea-sys <pea98258@gmail.com>
2023-05-07 07:43:17 +0900

    古典学派

---
+++ Unit-Testing/ch2/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch2
+{
+    public class Customer
+    {
+        public bool Purchase(Store store, Product product, int quantity)
+        {
+            if (!store.HasEnoughInventory(product, quantity))
+            {
+                return false;
+            }
+
+            store.RemoveInventory(product, quantity);
+
+            return true;
+        }
+    }
+}
