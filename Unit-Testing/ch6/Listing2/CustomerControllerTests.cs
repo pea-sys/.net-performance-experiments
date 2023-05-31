07eb0729b443a769ac8d1416fdd28d377332c453
pea-sys <pea98258@gmail.com>
2023-06-01 06:57:12 +0900

    状態値ベーステスト

---
+++ Unit-Testing/ch6/Listing2/CustomerControllerTests.cs
@@ -0,0 +1,21 @@
+﻿
+namespace ch6.Listing2
+{
+    /// <summary>
+    /// 状態ベーステストサンプル
+    /// </summary>
+    public class CustomerControllerTests
+    {
+        [Fact]
+        public void Adding_a_product_to_an_order()
+        {
+            var product = new Product("Hand wash");
+            var sut = new Order();
+
+            sut.AddProduct(product);
+
+            Assert.Equal(1, sut.Products.Count);
+            Assert.Equal(product, sut.Products[0]);
+        }
+    }
+}
---
+++ Unit-Testing/ch6/Listing2/Order.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ch6.Listing2
+{
+    public class Order
+    {
+        private readonly List<Product> _products = new List<Product>();
+        public IReadOnlyList<Product> Products => _products.ToList();
+
+        public void AddProduct(Product product)
+        {
+            _products.Add(product);
+        }
+    }
+}
