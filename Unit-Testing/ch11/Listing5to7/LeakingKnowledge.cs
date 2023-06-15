50b098651b612d2be18bbdd4f21f27c9f54a07ab
pea-sys <pea98258@gmail.com>
2023-06-16 06:17:00 +0900

    アンチパターン:ドメイン知識の漏洩

---
+++ Unit-Testing/ch11/Listing5to7/LeakingKnowledge.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch11.Listing5to7
+{
+    public static class Calculator
+    {
+        public static int Add(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+    }
+
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Adding_two_numbers()
+        {
+            int value1 = 1;
+            int value2 = 3;
+            int expected = value1 + value2;
+
+            int actual = Calculator.Add(value1, value2);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+    /// <summary>
+    /// 悪いテスト：expectedに実装が漏れている
+    /// </summary>
+    public class BadTests
+    {
+        [Theory]
+        [InlineData(1, 3)]
+        [InlineData(11, 33)]
+        [InlineData(100, 500)]
+        public void Adding_two_numbers(int value1, int value2)
+        {
+            int expected = value1 + value2;
+
+            int actual = Calculator.Add(value1, value2);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+
+    public class GoodTests
+    {
+        [Theory]
+        [InlineData(1, 3, 4)]
+        [InlineData(11, 33, 44)]
+        [InlineData(100, 500, 600)]
+        public void Adding_two_numbers(int value1, int value2, int expected)
+        {
+            int actual = Calculator.Add(value1, value2);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
