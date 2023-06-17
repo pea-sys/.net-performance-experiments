b7040d9e6c91b3353ea0e3e57a3419068c9dcef4
pea-sys <pea98258@gmail.com>
2023-06-18 06:27:14 +0900

    単体テストにおける現在日時の扱い

---
+++ Unit-Testing/ch11/Listing16to17/Ambient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch11.Listing16to17
+{
+    /// <summary>
+    /// プロダクションコードを汚す
+    /// </summary>
+    public static class DateTimeServer
+    {
+        private static Func<DateTime> _func;
+        public static DateTime Now => _func();
+
+        public static void Init(Func<DateTime> func)
+        {
+            _func = func;
+        }
+    }
+
+    /*
+    // Initialization code for production
+    DateTimeServer.Init(() => DateTime.UtcNow);
+
+    // Initialization code for unit tests
+    DateTimeServer.Init(() => new DateTime(2016, 5, 3));
+    */
+}
