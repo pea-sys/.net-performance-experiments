2ab9d00fcaa3dea573a75847fde7e9a9f99b1136
pea-sys <49807271+pea-sys@users.noreply.github.com>
2024-11-10 17:27:33 +0900

    feat: cachelineのベンチマーク

---
+++ Original/CachelineBench/CachelineBench/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+
+
+//| Method | Mean | Error | StdDev | Min | Max |
+//| ------------------ | ----------:| -----------:| ----------:| ----------:| ----------:|
+//| RowMajorAccess | 2.615 ms | 0.7164 ms | 0.0393 ms | 2.590 ms | 2.660 ms |
+//| ColumnMajorAccess | 17.801 ms | 30.6051 ms | 1.6776 ms | 16.113 ms | 19.468 ms |
+
+namespace CachelineBench
+{
+    [ShortRunJob]
+    [MinColumn, MaxColumn]
+    public class Measure
+    {
+        PerformanceCounter cacheCounter = new PerformanceCounter("Memory", "Cache Faults/sec");
+        const int Size = 1024; // 行列のサイズ
+        static double[,] matrix = new double[Size, Size];
+
+
+        [Benchmark]
+        public void RowMajorAccess()
+        {
+            cacheCounter.NextValue();
+
+            double sum = 0.0;
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    sum += matrix[i, j];
+                }
+            }
+
+            float cacheFaults = cacheCounter.NextValue();
+            Console.WriteLine($"Row Cache Faults/sec: {cacheFaults}");
+        }
+        [Benchmark]
+        public void ColumnMajorAccess()
+        {
+            cacheCounter.NextValue();
+            double sum = 0.0;
+            for (int j = 0; j < Size; j++)
+            {
+                for (int i = 0; i < Size; i++)
+                {
+                    sum += matrix[i, j];
+                }
+            }
+
+            float cacheFaults = cacheCounter.NextValue();
+            Console.WriteLine($"Column Cache Faults/sec: {cacheFaults}");
+        }
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            var summary = BenchmarkRunner.Run<Measure>();
+            Console.ReadKey();            
+        }
+    }
+}
---
+++ Original/CachelineBench/CachelineBench/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般的な情報は、次の方法で制御されます。
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更します。
+[assembly: AssemblyTitle("CachelineBench")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CachelineBench")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定します。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("72ee88c6-93ea-49d3-b38a-060091fa9caa")]
+
+// アセンブリのバージョン情報は次の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      リビジョン
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
