1a034c64deb3d4f99cceec72952f6761b365c954
pea-sys <49807271+pea-sys@users.noreply.github.com>
2024-12-05 07:08:14 +0900

    Add files via upload

---
+++ Original/TLBBench/TLBBench/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Disassemblers;
+using BenchmarkDotNet.Running;
+using CommandLine;
+
+//| Method | Mean | Error | StdDev | Min | Max |
+//| ----------------- | ---------:| ----------:| ---------:| ---------:| ---------:|
+//| AccessWithTLB | 15.16 ms | 3.800 ms | 0.208 ms | 15.02 ms | 15.40 ms |
+//| AccessWithoutTLB | 25.96 ms | 64.123 ms | 3.515 ms | 23.01 ms | 29.85 ms |
+
+namespace TLBBench
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Measure>();
+            Console.ReadKey();
+        }
+    }
+    [ShortRunJob]
+    [MinColumn, MaxColumn]
+    public class Measure
+    {
+        const int Size = 8192; // 配列のサイズ（ページサイズを意識する）
+        const int Stride = 64; // アクセス間のステップ(64の場合はTLB効果を活用)
+        static long[,] data = new long[Size, Size];
+        [Benchmark]
+        public void AccessWithTLB()
+        {
+            long sum = 0;
+            // 行優先アクセス：TLB最適化が効く
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j += Stride)
+                {
+                    sum += data[i, j];
+                }
+            }
+        }
+        [Benchmark]
+        public void AccessWithoutTLB()
+        {
+            long sum = 0;
+            // 列優先アクセス：TLB最適化が効きにくい
+            for (int j = 0; j < Size; j += Stride)
+            {
+                for (int i = 0; i < Size; i++)
+                {
+                    sum += data[i, j];
+                }
+            }
+        }
+    }
+}
---
+++ Original/TLBBench/TLBBench/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般的な情報は、次の方法で制御されます。
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更します。
+[assembly: AssemblyTitle("TLBBench")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TLBBench")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定します。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("addf7c5a-9734-4a81-a628-63ab6e55518c")]
+
+// アセンブリのバージョン情報は次の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      リビジョン
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
