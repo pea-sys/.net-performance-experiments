2b7e57072b85f64edcd83c9b55bc967bcb37ab23
pea-sys <49807271+pea-sys@users.noreply.github.com>
2024-12-31 16:45:13 +0900

    MemoryOrderingBench

---
+++ Original/MemoryOrderingBench/MemoryOrderingBench/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Disassemblers;
+using BenchmarkDotNet.Running;
+using CommandLine;
+
+//| Method | Mean | Error | StdDev | Min | Max |
+//| ------------------- | ----------:| ----------:| ---------:| ----------:| ----------:|
+//| NoMemoryOrdering | 61.98 ms | 0.923 ms | 0.051 ms | 61.93 ms | 62.03 ms |
+//| WithMemoryOrdering | 182.23 ms | 12.433 ms | 0.681 ms | 181.50 ms | 182.85 ms |
+
+namespace MemoryOrderingBench
+{
+    [ShortRunJob]
+    [MinColumn, MaxColumn]
+    public class Measure
+    {
+        const int iterations = 10000000; // 演算を行う回数
+        static int sharedValue = 0;
+
+        [Benchmark]
+        public void NoMemoryOrdering()
+        {
+            sharedValue = 0;
+            Parallel.For(0, iterations, i =>
+            {
+                sharedValue++;
+            });
+
+            Console.WriteLine($"Final shared value without ordering: {sharedValue}");
+        }
+        [Benchmark]
+        public void WithMemoryOrdering()
+        {
+            sharedValue = 0;
+            Parallel.For(0, iterations, i =>
+            {
+                Interlocked.Increment(ref sharedValue); // 原子性を保証する
+            });
+
+            Console.WriteLine($"Final shared value with correct synchronization: {sharedValue}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Measure>();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Original/MemoryOrderingBench/MemoryOrderingBench/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般的な情報は、次の方法で制御されます。
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更します。
+[assembly: AssemblyTitle("MemoryOrderingBench")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MemoryOrderingBench")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定します。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("ff8a2c3a-c9d5-4ea9-bd72-d7214625e8bb")]
+
+// アセンブリのバージョン情報は次の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      リビジョン
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
